1be8117aaa97b4d914f699c07053d2635d09f3df
bishopcanno <ericbispres@gmail.com>
2018-09-23 18:28:18 -0600

    built the landing page and it has mobile responsiveness, built the desktop side of the about page and immplemented a jquery function to find the screen size so that it too can have mobile responsiveness. I did this because it would have been too hard and messy to make it work with css. still need to build contact and portfolio pages

--- Assests/portfolioJS/portfolioScript.js
+++ Assests/portfolioJS/portfolioScript.js
@@ -1,3 +1,66 @@
+// Global JS
+
+// parallax functionality
 $(document).ready(function(){
     $('.parallax').parallax();
 });
+// Text Typing function
+function typeEffect(element, speed) {
+	var text = $(element).text();
+	$(element).html('');
+	
+	var i = 0;
+	var timer = setInterval(function() {
+        if (i < text.length) {
+            $(element).append(text.charAt(i));
+            i++;
+        } else {
+            clearInterval(timer);
+        }
+    }, speed);
+}
+
+// About Page JS---------------------------------------------------------------------------------------------------------About Page JS
+
+// funtion for determing if the page should load in mobile or desktop
+function windowSizing(width){
+    if (width <= 800 ){
+        $("#about-desktop-container").hide();
+    } else{
+        $("#about-mobile-container").hide();
+    }
+}
+
+// Returns width of browser viewport, will need for mobile responsiveness
+var winWidth = $( window ).width();
+
+// implementation of windowSizing Function
+windowSizing(winWidth);
+
+// arrays for parallax images
+var profileImgArray = [];
+var activityImgArray = [];
+
+// randomized images pulled from the arrays to push to the parallax's
+var profileRandomImg;
+var activityRandomImg;
+
+// pushing the random images up to the parallax's
+$("#parallaxOne").attr("src", profileRandomImg);
+$("#parallaxTwo").attr("src", activityRandomImg);
+
+
+// Portfolio Page JS------------------------------------------------------------------------------------------------------Portfolio Page JS
+
+// text typing implementation Portfolio Page
+$( document ).ready(function() {
+  var delay = 200;
+  var speed = 50;
+  typeEffect($('.h1-type'), delay);
+  setTimeout(function(){
+    $('.about-pear').css('display', 'inline-block');
+    typeEffect($('.about-pear'), speed);
+  }, delay);
+});
+
+// Contact Page JS--------------------------------------------------------------------------------------------------------Contact Page JS
