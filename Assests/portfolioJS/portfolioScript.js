f8c56751c486ba8b4ff424f0ce57836ed969814f
bishopcanno <ericbispres@gmail.com>
2018-09-26 12:42:28 -0600

    Finished off the main structure of landing, portfolio, about and contact pages. Styling is about where i want it, maybe room for a little improvement. Still need to link up more of my homework assignments and try to get them to look better. need to link up my linked in and start trying to build connections there. need to comment contact page related code. Still need to write better description about myself in the about me page. lastly put in a footer

--- Assests/portfolioJS/portfolioScript.js
+++ Assests/portfolioJS/portfolioScript.js
@@ -104,3 +104,29 @@ $(document).ready(function () {
 
 
 // -----------------------------------------------------------------------------CONTACT PAGE JS---------------------------------------------------------------------------------------------------
+
+
+// Js for the transparent text over image
+$(document).ready(function(){
+    var mouseX, mouseY;
+    var ww = $( window ).width();
+    var wh = $( window ).height();
+    var traX, traY;
+    $(document).mousemove(function(e){
+      mouseX = e.pageX;
+      mouseY = e.pageY;
+      traX = ((6 * mouseX) / 570) + 40;
+      traY = ((6 * mouseY) / 570) + 50;
+    //   console.log(traX);
+      $(".title").css({"background-position": traX + "%" + traY + "%"});
+    });
+  });
+
+// collapsable initialization
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+    var elem = document.querySelector('.collapsible.expandable');
+    var instance = M.Collapsible.init(elem, {
+    accordion: true
+});
+  });
