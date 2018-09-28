94b363882d5c2d3df441925feb82da75cb3e77cf
bishopcanno <ericbispres@gmail.com>
2018-09-28 09:05:14 -0600

    changed the name of this project to Eric_B's_Portfolio_Page. I changed it so that each page would have their own js files just like they have their own css files. No need to have a bunch of script fire off that won't be sused

--- Assests/portfolioJS/portfolioScript.js
+++ Assests/portfolioJS/portfolioScript.js
@@ -1,11 +1,5 @@
-// -------------------------------------------------------------------------GLOBAL JS------------------------------------------------------------------------------------
-
+// -----------------------------------------------------------------------------PORTFOLIO PAGE JS-----------------------------------------------------------------------------------
 console.log("js file is linked");
-
-// parallax functionality
-$(document).ready(function(){
-    $('.parallax').parallax();
-});
 
 // Text Typing function
 function typeEffect(element, speed) {
@@ -22,57 +16,6 @@ function typeEffect(element, speed) {
         }
     }, speed);
 }
-
-// ---------------------------------------------------------------------------ABOUT PAGE JS-----------------------------------------------------------------------
-
-// Returns width of viewport, will need for mobile responsiveness
-var winWidth = $( window ).width();
-
-// funtion for determing if the page should load in mobile or desktop
-function windowSizing(width){
-    if (width <= 900 ){
-         // hides the desktop header and the parallaxs because the screen is too small to view the full site
-        $("#desktop-header").hide();
-        $(".parallax-container").hide();
-    } else{
-        // string array that is the relative path for profile images that will be pushed to the first parallax
-        var profileImgArray = ['./Assests/Images/alpineLake.jpg', './Assests/Images/coolDude.jpg', './Assests/Images/ericBandit.jpg', './Assests/Images/greatDay.jpg', 
-            './Assests/Images/jacksonLove.jpg', './Assests/Images/jamesEric.jpg', './Assests/Images/katieEricsBday.jpg', './Assests/Images/liamKayden.jpg', 
-            './Assests/Images/regulatorJohnson.jpg'];
-        
-        // string array that is the relative path for activity images that will be pushed to the second parallax
-        var activityImgArray = ['./Assests/Images/actionBandit.jpg', './Assests/Images/actionJames.jpg', './Assests/Images/banditBackpack.jpg', './Assests/Images/banditIdaho.jpg',
-            './Assests/Images/bestFriends.jpg', './Assests/Images/cabinFront.jpg', './Assests/Images/happyBandit.jpg', './Assests/Images/sanFranRain.jpg',
-            './Assests/Images/solidtudeCayman.jpg'];
-
-        // randomized number of the image array
-        var profileRandomNum = [Math.floor(Math.random() * profileImgArray.length)];
-       
-        // gets random string of the profile array to push to parallax one
-        var profileRandomImg = profileImgArray[profileRandomNum];
-        
-        // randomized number of the image array
-        var activityRandomNum = [Math.floor(Math.random() * activityImgArray.length)];
-        
-        // gets random string from the activity array to push to parallax two
-        var activityRandomImg = activityImgArray[activityRandomNum];
-
-        // pushing the random images up to the parallax's
-        $("#parallaxOne").attr("src", profileRandomImg);
-        $("#parallaxTwo").attr("src", activityRandomImg);
-        
-        // embedded the whole parallax randomizaton inside the else so that it only implements when its needed
-        
-        // hides the mobile header because the screen is large enough to view the full site
-        $("#mobile-header").hide();
-    }
-}
-
-// implementation of windowSizing Function
-windowSizing(winWidth);
-
-
-// -----------------------------------------------------------------------------PORTFOLIO PAGE JS-----------------------------------------------------------------------------------
 
 // text typing implementation Portfolio Page
 $( document ).ready(function() {
@@ -101,32 +44,3 @@ $(document).ready(function () {
         },
       });
 });
-
-
-// -----------------------------------------------------------------------------CONTACT PAGE JS---------------------------------------------------------------------------------------------------
-
-
-// Js for the transparent text over image
-$(document).ready(function(){
-    var mouseX, mouseY;
-    var ww = $( window ).width();
-    var wh = $( window ).height();
-    var traX, traY;
-    $(document).mousemove(function(e){
-      mouseX = e.pageX;
-      mouseY = e.pageY;
-      traX = ((6 * mouseX) / 570) + 40;
-      traY = ((6 * mouseY) / 570) + 50;
-    //   console.log(traX);
-      $(".title").css({"background-position": traX + "%" + traY + "%"});
-    });
-  });
-
-// collapsable initialization
-$(document).ready(function(){
-    $('.collapsible').collapsible();
-    var elem = document.querySelector('.collapsible.expandable');
-    var instance = M.Collapsible.init(elem, {
-    accordion: true
-});
-  });
